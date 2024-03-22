fb3fb8cd398176605cd9f5d9a48ebdbec220ce7c
Narutayusu <kamondach.yusu@bumail.net>
2024-03-22 18:01:32 +0700

    Score And map

---
+++ Assets/Script/ScoreCount.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+    public Transform player;
+    public TMP_Text scoretext;
+
+    private void Update()
+    {
+        scoretext.text = player.position.z.ToString();
+    }
+}
+
+
+
